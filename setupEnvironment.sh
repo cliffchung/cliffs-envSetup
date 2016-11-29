2abf7692318c37cdac0ec48d42580c94b67bdd84
Cliff Chung <ctschung@gmail.com>
2016-11-28 19:52:22 -0800

    Minor Plugin Install change

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -71,7 +71,7 @@ copyDotDirectories() {
 setupVimPlugins() {
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    vim +PluginInstall +qall
+    vim +PlugInstall +qall
 }
 
 installOhMyZsh() {
